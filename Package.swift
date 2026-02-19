e7831a8ee6fd377d8f34a6848dd8d4e4b8185aaf
Dario Pellegrini <dario.pellegrini@starktechnologypartner.com>
2026-02-19 09:46:25 +0100

    Added SPM

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "StorageDone-iOS",
-    platforms: [
-        .iOS(.v14)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
