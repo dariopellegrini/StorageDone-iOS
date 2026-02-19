856c306e35a61fa9bbd9f84b5318477ec5ede1cb
Dario Pellegrini <dario.pellegrini@starktechnologypartner.com>
2026-02-19 09:49:13 +0100

    Added mimimum iOS

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "StorageDone-iOS",
+    platforms: [
+        .iOS("15.0")
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -26,8 +29,6 @@ let package = Package(
             name: "StorageDone",
             dependencies: ["CouchbaseLiteSwift", "RxSwift"],
             path: "StorageDone",
-            exclude: ["Info.plist"]
-        )
-    ]
-)
+            exclude: ["Info.plist"])
+    ])
 
